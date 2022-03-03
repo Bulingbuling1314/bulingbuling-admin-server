3ece95740ff1f215084d42086a7d720b41e3e7f1
ZhangAoQi <15600057632@163.com>
2022-03-03 19:25:46 +0800

    递归查询子菜单

--- src/main/java/com/bulingbuling/admin/server/user/service/MenuService.java
+++ src/main/java/com/bulingbuling/admin/server/user/service/MenuService.java
@@ -1,6 +1,5 @@
 package com.bulingbuling.admin.server.user.service;
 
-import cn.hutool.core.lang.Console;
 import cn.hutool.json.JSONUtil;
 import com.bulingbuling.admin.server.tools.RedisService;
 import com.bulingbuling.admin.server.user.dao.MenuDo;
@@ -12,10 +11,8 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Service
 public class MenuService {
@@ -34,46 +31,21 @@ public class MenuService {
         List<MenuEntity> menuList = menuDo.findByRole((String) role);
 
         List<MenuEntityResult> resultList = new ArrayList<>();
-        this.initMenu(menuList,0,resultList,null);
+        this.initMenu(menuList, parentId, resultList, null);
 
         return resultList;
-        //return formatMenu(menuList, resultList);
-//        return menuDo.findAll();
     }
 
-    public void initMenu(List<MenuEntity> menuList,int parentId,List<MenuEntityResult> resultList,MenuEntityResult parent){
-        menuList.stream().filter(menu->menu.getParentId()==parentId).forEach(menu->{
+    public void initMenu(List<MenuEntity> menuList, int parentId, List<MenuEntityResult> resultList, MenuEntityResult parent) {
+        menuList.stream().filter(menu -> menu.getParentId() == parentId).forEach(menu -> {
             MenuEntityResult result = new MenuEntityResult();
-            BeanUtils.copyProperties(menu,result);
-            if(parent==null){
+            BeanUtils.copyProperties(menu, result);
+            if (parent == null) {
                 resultList.add(result);
-            }else{
+            } else {
                 parent.addChildren(result);
             }
-            initMenu(menuList,result.getId(),null,result);
+            initMenu(menuList, result.getId(), null, result);
         });
-    }
-
-
-
-
-
-
-    private List<MenuEntityResult> formatMenu(List<MenuEntityResult> menuList, List<MenuEntityResult> resultList) {
-        // 1: 目录 2: 菜单
-        for (MenuEntity result : menuList) {
-            // 子集
-            MenuEntityResult data = new MenuEntityResult();
-            BeanUtils.copyProperties(result, data);
-            if (result.getType() == 1) {
-                List<MenuEntityResult> menuChilds = menuDo.findByRoleAndParentId(result.getRole(), result.getId());
-                if (menuChilds != null && menuChilds.size() > 0) {
-                    data.setChildren(menuChilds);
-                    formatMenu(menuChilds, resultList);
-                }
-            }
-            resultList.add(data);
-        }
-        return resultList;
     }
 }
