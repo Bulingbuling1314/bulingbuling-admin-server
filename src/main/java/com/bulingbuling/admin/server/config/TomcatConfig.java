0a4ed0a1a08b069fd1013fcf32ec4a0a293f8f8a
Bulingbuling1314 <Bulingbuling1314@163.com>
2023-02-10 15:07:46 +0800

    https

---
+++ src/main/java/com/bulingbuling/admin/server/config/TomcatConfig.java
@@ -0,0 +1,41 @@
+package com.bulingbuling.admin.server.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfig {
+    @Bean
+    public Connector connector(){
+        Connector connector=new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        // 设置监听端口
+        connector.setPort(8099);
+        connector.setSecure(false);
+        // 当浏览器监听到http端口（默认为80，此处为8080）时，会自动重定向到443端口（与项目端口保持一致）
+        connector.setRedirectPort(8098);
+        return connector;
+    }
+
+    @Bean
+    public TomcatServletWebServerFactory tomcatServletWebServerFactory(Connector connector){
+        TomcatServletWebServerFactory tomcat=new TomcatServletWebServerFactory(){
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint=new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection=new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(connector);
+        return tomcat;
+    }
+}
